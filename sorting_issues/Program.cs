9fbc55ee9ae62d27f07e69036e49962628092568
dx1 <dikeox11@gmail.com>
2026-01-26 12:49:18 -0600

    moderated for 3 types of sorting methods

--- sorting_issues/Program.cs
+++ sorting_issues/Program.cs
@@ -6,159 +6,74 @@ namespace sorting_issues
 	{
 		static void Main(string[] args)
 		{
-			int[] sortArrayBySelection = { 2, 10, -3, -7, 11, 5, 8, 18, 1, 27, -32, -99};
-			SelectionSortMethod(sortArrayBySelection);
 
             int[] sortArrayByMerge = { 12, 1, -3, -17, 6, 15, -8, 18, 10, 37, 20, -99 };
             MergeSortMethod(sortArrayByMerge);
 
-            int[] sortArrayByInsertion = { 22, 100, -13, -27, 1, 25, -8, -18, -1, 47, 16, -99 };
-            InsertionSortMethod(sortArrayByInsertion);
-
-			int[] sortArrayByQuick = { -9, -12, -4, 19, 3, 0, 8, 14, 9, 20 };
-			QuickSort(sortArrayByQuick);
-
-			Console.WriteLine("This is  for selection sorting with the while loop!");
-			Console.WriteLine(string.Join(" / ", sortArrayBySelection));
-
-			Console.WriteLine("\n");
-
 			Console.WriteLine("This is for Merge sorting with the while loop");
 			Console.WriteLine(string.Join(" + ", sortArrayByMerge));
 
-            Console.WriteLine("\n");
-            Console.WriteLine("This is for Insertion sorting with the for and while loop");
-            Console.WriteLine(string.Join(" * ", sortArrayByInsertion));
-
-            Console.WriteLine("\n");
-            Console.WriteLine("This is for quick sorting");
-            Console.WriteLine(string.Join(" >> ", sortArrayByInsertion));
 
             Console.ReadKey();
 		}
 
-		static void SelectionSortMethod(int[] x)
-		{
-			int i = 0;
-			while (i < x.Length - 1)
-			{
-				int minValue = x[i];
-				int minIndex = i;
-
-				int j = i + 1;
-				while(j < x.Length)
-				{
-					if (x[j] < minValue)
-					{
-						minValue = x[j];
-						minIndex = j;
-					}
-					j++;
-				}
-				(x[i], x[minIndex]) = (x[minIndex], x[i]);
-                i++;
-            }
-		}
 		
-		static void InsertionSortMethod(int[] y)
-		{
-            // The first element is assumed to be already sorted
-            // We start from the second element and insert it into the sorted part
-            for (int i = 1; i < y.Length; i++)
-			{
-                // j tracks the position of the element as it moves left
-                int j = i ;
-                // Move the element left while it is smaller than the previous one
-                while (j > 0 && y[j] < y[j - 1])
-				{
-					//then swap
-					(y[j], y[j - 1]) = (y[j - 1], y[j]);
-
-                    // Move left to check if it needs to go further
-                    j--;
-				}
-			}
-		}
-
-		//For the next sorting method: Please note that the wermacht, AMGN, AMGS, Blitzkreig, heer words
-		//Are just mere data containers for learning and no reference nor interest in
-		//Nazi Germany army of 1930s. this is just for pure learning interest
-
-		static void MergeSortMethod(int[] heer)
+		//THIS PROGRAM IS A SORTING MECHANIC FOR
+		//1. MERGE SORT
+		//2. QUICK SORT
+		//3. BUBBLE SORT 
+		
+		static void MergeSortMethod(int[] arrayForMergeSort)
 		{
 			//Create a base case to terminate the recursive method
-			if (heer.Length <= 1) return;
+			if (arrayForMergeSort.Length <= 1) return;
 
-			//Find the midpoint
-			int blitzkreig = heer.Length / 2;
-			//Append midpoint to two aux unitsint
+			//Find the midpoint of the parent array
+			int midpoint = arrayForMergeSort.Length / 2;
 
-			int[] ArmyGroupNorth = Wehrmachtsgliederung(heer, 0, blitzkreig - 1);
-			int[] ArmyGroupSouth = Wehrmachtsgliederung(heer, blitzkreig, heer.Length - 1);
+			//Append midpoint to two aux arrays : using Divide and Conquer
+			int[] arrayGroupA = SubArrayMethod(arrayForMergeSort, 0, midpoint - 1);
+			int[] arrayGroupB = SubArrayMethod(arrayForMergeSort, midpoint, arrayForMergeSort.Length - 1);
 
-			//Break the array further
-			MergeSortMethod(ArmyGroupNorth);
-			MergeSortMethod(ArmyGroupSouth);
+			//Break the array further until it is having just 1 element in each array
+			MergeSortMethod(arrayGroupA);
+			MergeSortMethod(arrayGroupB);
 
-			//Now time to sort back together
-			//create aux for groups and parentgroup
+			//Now time to append elements back together
+			//create aux int for all arrays and 
 			int a = 0, b = 0, c = 0;
-			while(a < ArmyGroupNorth.Length && b < ArmyGroupSouth.Length)
+			while(a < arrayGroupA.Length && b < arrayGroupB.Length)
 			{
-				if (ArmyGroupNorth[a] < ArmyGroupSouth[b])
+				if (arrayGroupA[a] < arrayGroupB[b])
 				{
-					//Append the smallest into the parentgrouppe
-					heer[c++] = ArmyGroupNorth[a++];
+					//Append the smallest into the parent array
+					arrayForMergeSort[c++] = arrayGroupA[a++];
 				}
 				else
 				{
-					heer[c++] = ArmyGroupSouth[b++];
-				}
+                    arrayForMergeSort[c++] = arrayGroupB[b++];
+                }
 			}
 
-			while(a < ArmyGroupNorth.Length)
+			while(a < arrayGroupA.Length)
 			{
-				heer[c++] = ArmyGroupNorth[a++];
-			}
+                arrayForMergeSort[c++] = arrayGroupA[a++];
+            }
 			
-			while(b < ArmyGroupSouth.Length)
+			while(b < arrayGroupB.Length)
 			{
-				heer[c++] = ArmyGroupSouth[b++];
-			}
+                arrayForMergeSort[c++] = arrayGroupB[b++];
+            }
 
         }
 
-		static int[] Wehrmachtsgliederung(int[] x, int Anfang, int Ende)
+		static int[] SubArrayMethod(int[] x, int Anfang, int Ende)
 		{
 			int[] resultat = new int[Ende - Anfang + 1];
 			Array.Copy(x, Anfang, resultat, 0, Ende - Anfang + 1);
 			return resultat;
 		}
-
-		//Quick Sort
-		static void QuickSort (int[] z)
-		{
-			SortPart(z, 0, z.Length - 1);
-		}
-
-		static void SortPart(int[] z, int lowerIndices, int upperIndices)
-		{
-			if (lowerIndices >= upperIndices) return;
-			int pivot = z[upperIndices];
-			int j = lowerIndices - 1;
-			for(int i = lowerIndices; i < upperIndices; i++)
-			{
-				if (z[i] < pivot)
-				{
-					j++;
-					(z[j], z[i]) = (z[i], z[j]);
-				}
-			}
-			int p = j + 1;
-			(z[p], z[upperIndices]) = (z[upperIndices], z[p]);
-			SortPart(z, lowerIndices, p - 1);
-			SortPart(z, p + 1, upperIndices);
-		}
+		
 
     }
 }
